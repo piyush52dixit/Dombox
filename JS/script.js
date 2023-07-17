b0e468c07cfd2ce6af8d55004aaf18984fa83b26
piyushongit <piyushdixit567@gmail.com>
2023-07-17 11:45:56 +0530

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+// if (js === "amazing") alert("js is fun");
+console.log("hello world!");
+
+let country = "India";
+
+let population = "145";
+
+// Data Types
+
+let isIsland = "false";
+let language = "Hindi";
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+const continent = "Asia";
+console.log(continent);
